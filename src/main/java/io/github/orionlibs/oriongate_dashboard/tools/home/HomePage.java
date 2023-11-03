6a6ef0ed90b6ba041ec270d7b0f8a3b50fe8204d
Dimitrios Efthymiou <efthymiou.dimitrios1@gmail.com>
2023-11-03 15:19:19 +0000

    clean up

--- src/main/java/io/github/orionlibs/oriongate_dashboard/tools/home/HomePage.java
+++ src/main/java/io/github/orionlibs/oriongate_dashboard/tools/home/HomePage.java
@@ -2,8 +2,6 @@ package io.github.orionlibs.oriongate_dashboard.tools.home;
 
 import io.github.orionlibs.oriongate_dashboard.Page;
 import java.awt.BorderLayout;
-import java.awt.Graphics;
-import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 public class HomePage extends Page
@@ -24,12 +22,7 @@ public class HomePage extends Page
 
     public void loadHomePage() throws IOException
     {
-        /*HomePagePanel homePage = new HomePagePanel();
-        homePage.setLayout(new BorderLayout());
-        //homePage.add(javafxPanel, BorderLayout.CENTER);
-        addPanel(homePage);
-        revalidate();
-        repaint();*/
+        //HomePagePanel homePage = new HomePagePanel();
         setNewScene("/configuration/pages/home/home.html");
     }
 
@@ -37,17 +30,6 @@ public class HomePage extends Page
     public void loadHome2Page() throws IOException
     {
         //Home2PagePanel home2Page = new Home2PagePanel();
-        //home2Page.setLayout(new BorderLayout());
-        //home2Page.add(javafxPanel, BorderLayout.CENTER);
-        //getComponent(0).paint(home2Page.getGraphics());
-        //getComponent(0).invalidate();
-        //getComponent(0).setEnabled(false);
-        //getComponent(0).update(home2Page.getGraphics());
-        //getComponent(0).revalidate();
-        //getComponent(0).repaint();
-        //addPanel(home2Page);
-        //revalidate();
-        //repaint();
         setNewScene("/configuration/pages/home/home2.html");
     }
 }
